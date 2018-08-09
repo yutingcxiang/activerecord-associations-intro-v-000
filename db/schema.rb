b4a57aacc953e556d7fa4df347f2978abd0ecd76
Christine Xiang <yutingcxiang@gmail.com>
2018-08-09 18:46:21 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 4) do
+ActiveRecord::Schema.define(version: 3) do
 
   create_table "artists", force: :cascade do |t|
     t.string "CreateArtists"
