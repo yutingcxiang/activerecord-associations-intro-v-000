f44a7f28d9d267ed3efac4ad1b991a0e371bd17b
Christine Xiang <yutingcxiang@gmail.com>
2018-08-09 18:45:35 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
+ActiveRecord::Schema.define(version: 4) do
 
   create_table "artists", force: :cascade do |t|
     t.string "CreateArtists"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 2) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string "name"
+    t.integer "artist_id"
+    t.integer "genre_id"
   end
 
 end
